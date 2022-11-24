94c38572317875745b28cb96a46ae1a5e1f740ca
Profsain <profsaincrown@gmail.com>
2022-11-24 20:04:42 +0000

    Added model validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,12 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  # validation
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
+
+
   # update post counter
   def increase_post_counter
     user.posts_counter += 1
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  # validation
+  validates :name, presence: true
+  validates :posts_count, numericality: { greater_than_or_equal_to: 0 }
+
+
   # Return 3 most recent users post
   def recent_posts
     posts.order(created_at: :desc).limit(3)
