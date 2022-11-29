8fba34954f4d3d96884a5da90d6d7e4af0e9883d
Profsain <profsaincrown@gmail.com>
2022-11-29 14:58:43 +0000

    Created request spec

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -6,5 +6,32 @@ RSpec.describe 'Posts', type: :request do
       get '/posts/index'
       expect(response).to have_http_status(:success)
     end
+
+    it 'renders the index template' do
+      get '/posts/index'
+      expect(response).to render_template('index')
+    end
+
+    it 'contains the correct heading' do
+      get '/posts/index'
+      expect(response.body).to include('Post List')
+    end
+  end
+
+  describe 'GET /show' do
+    it 'returns http success' do
+      get '/users/show/posts/show'
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the show template' do
+      get '/users/show/posts/show'
+      expect(response).to render_template('show')
+    end
+
+    it 'contains the correct heading' do
+      get '/users/show/posts/show'
+      expect(response.body).to include('Single Post Details Page')
+    end
   end
 end
