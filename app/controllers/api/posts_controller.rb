114bd0962e05036e6b1527bf86dbaf684668e8f8
iamyunusali <aliyunus291@gmail.com>
2022-12-08 23:14:50 +0300

    Add pots contoller for API

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,7 @@
+class Api::PostsController < Api::ApiController
+    def index
+    @user = User.find(params[:user_id])
+    @posts = @user ? @user.posts : Post.all.includes(:comments)
+    render json: @posts
+    end
+  end
