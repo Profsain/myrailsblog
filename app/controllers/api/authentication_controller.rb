0f0eba07f82a48168b8f814f59efd7b93dc005ed
Profsain <profsaincrown@gmail.com>
2022-12-09 00:22:19 +0000

    Implement changes required

--- app/controllers/api/authentication_controller.rb
+++ app/controllers/api/authentication_controller.rb
@@ -1,6 +1,7 @@
 class Api::AuthenticationController < Api::ApiController
   skip_before_action :authenticate_token!
 
+  # Authenticate user and return token
   def create
     user = User.find_by(email: params[:user][:email])
     if user.valid_password? params[:user][:password]
