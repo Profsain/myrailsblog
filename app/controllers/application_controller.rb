669b6aa051e438bb120df8197b16f4fd8839635a
Profsain <profsaincrown@gmail.com>
2023-02-01 06:30:12 +0000

    Implement changes required

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,6 @@
 class ApplicationController < ActionController::Base
-  skip_before_action :verify_authenticity_token
-  before_action :update_allowed_parameters, if: :devise_controller?
-
-  # prevent users to perform action without authentication
-  before_action :authenticate_user!
-
-  protected
-
-  def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :password_confirmation) }
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :current_password) }
+  def current_user
+    User.first
   end
+  
 end
