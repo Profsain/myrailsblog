e6881a409a1598466bc820a8b421083586dfb8f6
Profsain <profsaincrown@gmail.com>
2023-02-04 01:33:00 +0000

    fixed test error

--- spec/system/post_show_spec.rb
+++ spec/system/post_show_spec.rb
@@ -1,61 +1,26 @@
 require 'rails_helper'
-RSpec.describe 'Posts show page', type: :system do
+RSpec.describe 'Posts/show', type: :system do
   before do
     driven_by(:rack_test)
   end
 
-  before(:all) do
-    @user = User.includes(:posts, :comments).first
-    @post = @user.posts.first
-  end
-
-  it "shows post's title" do
-    visit "/users/#{@user.id}/posts/#{@post.id}"
-
-    expect(page).to have_text(@post.title)
-  end
-
-  it "shows post's author" do
-    visit "/users/#{@user.id}/posts/#{@post.id}"
-
-    expect(page).to have_content(@user.name)
-  end
-
-  it "shows post's number of commments" do
-    visit "/users/#{@user.id}/posts/#{@post.id}"
-
-    expect(page).to have_text(@post.comments_counter)
-  end
-
-  it "shows post's number of likes" do
-    visit "/users/#{@user.id}/posts/#{@post.id}"
-
-    expect(page).to have_text(@post.likes_counter)
-  end
-
-  it 'shows post body' do
-    visit "/users/#{@user.id}/posts/#{@post.id}"
-
-    expect(page).to have_text(@post.text)
-  end
-
-  it 'shows username of each commentor' do
-    visit "/users/#{@user.id}/posts/#{@post.id}"
-
-    if @post.comments_counter > 0
-      comment = @post.comments.first.author
-
-      expect(page).to have_text(comment.name)
+  describe 'Show' do
+    before(:all) do
+      @user = User.first
+      @posts = @user.posts
     end
-  end
 
-  it 'show comments of commentors' do
-    visit "/users/#{@user.id}/posts/#{@post.id}"
+    it 'shows the title, text, number of commment, number of likes and post author' do
+      visit user_posts_path(@user)
 
-    if @post.comments_counter > 0
-      comment = @post.comments.first
-
-      expect(page).to have_text(comment.text)
+      @posts.each do |post|
+        expect(page).to have_text(post.title)
+        expect(page).to have_text(post.text)
+        expect(page).to have_text(post.comments_counter)
+        expect(page).to have_text(post.likes_counter)
+        expect(page).to have_text(post.user.name)
+        
+      end
     end
   end
 end
